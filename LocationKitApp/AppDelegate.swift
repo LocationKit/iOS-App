d05cc608a43f9af6feb503defa346e16745d09c7
Michael Sanford <msanford@flipside5.com>
2015-11-22 11:25:47 -0500

    code cleanup

--- LocationKitApp/AppDelegate.swift
+++ LocationKitApp/AppDelegate.swift
@@ -14,9 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, LKLocationManagerDelegate
 
     static let locationHistoryDidChangeNotificationName = "locationHistoryDidChange"
 
+    // Location History
     var allLocationItems: [DayLocationInfo] = []
+    
+    // Advanced Location Manager
     private(set) var locationManager: LKLocationManager!
 
+    // MARK: Settings
     private let trackingKey = "com.socialradar.LocationKitApp.trackingEnabled"
     var trackingEnabled: Bool {
         get {
@@ -36,6 +40,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate, LKLocationManagerDelegate
         }
     }
     
+    // MARK: LKLocationManagerDelegate
+    func locationManager(manager: LKLocationManager, didStartVisit visit: LKVisit) {
+        addLocationItem(LocationItem(visit: visit))
+    }
+    
+    func locationManager(manager: LKLocationManager, didEndVisit visit: LKVisit) {
+        updateLocationVisit(visit)
+    }
+    
+    // MARK UIApplicationDelegate
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
@@ -53,36 +67,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, LKLocationManagerDelegate
         }
         
         return true
-    }
-    
-    func locationManager(manager: LKLocationManager, didStartVisit visit: LKVisit) {
-        addLocationItem(LocationItem(visit: visit))
-    }
-    
-    func locationManager(manager: LKLocationManager, didEndVisit visit: LKVisit) {
-        updateLocationVisit(visit)
-    }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
 }
