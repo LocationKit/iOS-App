35090b3c43e2f57bd9df59fb7ef9bd576ad6ed92
Victor Quinn <mail@victorquinn.com>
2015-11-30 16:11:21 -0500

    Update to latest CocoaPod, re-enable bitcode

--- Pods/LocationKit/LocationKit.framework/Headers/LKLocationManager.h
+++ Pods/LocationKit/LocationKit.framework/Headers/LKLocationManager.h
@@ -272,10 +272,52 @@ typedef NS_OPTIONS(NSUInteger, LKActivityMode) {
  *     to the settings it had prior to starting visit monitoring.
  */
 - (void)stopMonitoringVisits;
+
+/*
+ * startMonitoringVisitsWithFilter
+ *
+ * Discussion:
+ *     By default, when you start monitoring visits (by calling startMonitoringVisits()), your delegate
+ *     will get called for didStartVisit and didEndVisit for all visits to any location. However,
+ *     using this method, you can request that it be called only for a subset of locations. For
+ *     instance, rather than getting notified of all visits, you can specify a filter such as "Breweries"
+ *     or "CVS Pharmacy" to get notified only of visits to breweries or to a CVS.
+ *
+ *     This can be called multiple times with different filters and each filter is added.
+ *
+ *     Once a filter has been specified, didStartVisit and didEndVisit will stop getting called
+ *     for any visits not matching your filter.
+ */
 - (void)startMonitoringVisitsWithFilter:(LKVisitFilter* _Nonnull)filter;
+
+/*
+ * stopMonitoringVisitsWithFilter
+ *
+ * Discussion:
+ *     Call this with a filter to stop monitoring for visits that match that filter.
+ */
 - (void)stopMonitoringVisitsWithFilter:(LKVisitFilter* _Nonnull)filter;
 
+/*
+ * startMonitoringForRegion
+ *
+ * Discussion:
+ *     Call this with an LKRegion to start monitoring for the user to enter or exit the region.
+ *     When creating an LKRegion, you can specify a radius and a venue name or category and
+ *     LocationKit will automatically notify your app when your users get within your supplied
+ *     radius of any venue matching that filter.
+ *
+ *     For instance, you could specify a region with a radius of 100 and a venue name of "Starbucks
+ *     Coffee" to get notified whenever your users are within 100 meters of a Starbucks.
+ */
 - (void)startMonitoringForRegion:(CLRegion* _Nonnull)region;
+
+/*
+ * stopMonitoringForRegion
+ *
+ * Discussion:
+ *     Call this with an LKRegion to stop monitoring for entry and exit to that type of region.
+ */
 - (void)stopMonitoringForRegion:(CLRegion* _Nonnull)region;
 
 
@@ -302,10 +344,44 @@ typedef NS_OPTIONS(NSUInteger, LKActivityMode) {
  */
 - (void)optOut:(void (^ _Nonnull)(NSError* _Nullable))handler;
 
+/*
+ * setOperationMode
+ *
+ * Discussion:
+ *     Used to override the default operating mode of LocationKit. Supply an LKSetting specifying
+ *     how to override.
+ *
+ *     For instance, if you start visit monitoring, LocationKit will by default throttle the
+ *     GPS up when it detects the user is walking (and thus likely to visit a place of interest)
+ *     and down when it detects the user is still or driving (as they're not likely to visit
+ *     places when their phone is not moving at all or if it's moving at a high rate of speed).
+ *     However, some use cases require higher GPS accuracy while driving or periodically event
+ *     while the device is not moving so this method is added to allow for overriding the default
+ *     behavior.
+ *
+ *     This method pairs nicely with the delegate method `willChangeActivityMode` which will be
+ *     called whenever the user's activity mode is about to change (from still to walking or
+ *     from walking to driving)
+ */
 - (void)setOperationMode:(LKSetting* _Nonnull)setting;
 
+/*
+ * pause
+ *
+ * Discussion:
+ *     As its name would imply, this pauses LocationKit. This can be potentially dangerous to
+ *     call depending on your use case because it will stop LocationKit from continuing its
+ *     background operation so you will not continue to get passive visit detection or
+ *     background updates while paused.
+ */
 - (void)pause;
 
+/*
+ * resume
+ *
+ * Discussion:
+ *     Resume from being paused.
+ */
 - (NSError* _Nullable)resume;
 
 
@@ -313,11 +389,42 @@ typedef NS_OPTIONS(NSUInteger, LKActivityMode) {
 
 NS_ASSUME_NONNULL_BEGIN
 
+/*
+ * LKLocationManagerDelegate protocol
+ *
+ * Discussion:
+ *     Our delegate protocol -- very similar to Apple's CLLocationManagerDelegate protocol by
+ *     design, but with our LocationKit additions.
+ */
 @protocol LKLocationManagerDelegate <NSObject>
 
 @optional
 
-// Methods for parity with CLLocationManagerDelegate
+/*
+ * Methods for parity with CLLocationManagerDelegate
+ *
+ * Discussion:
+ *     This delegate includes all the base methods as the CLLocationManagerDelegate. For simplicity,
+ *     we have added comments only to the methods below which LocationKit modifies or tweaks.
+ *
+ *     For detailed documentation on the unmodified "inherited" methods, check out Apple's docs
+ *     for the CLLocationManagerDelegate here: http://vq.io/1lUIZXr
+ */
+
+/*
+ * didUpdateLocations
+ *
+ * Discussion:
+ *     This delegate method will be called whenever a new location point is obtained by the
+ *     device which is outside the specified distanceFilter. In LocationKit's default operation
+ *     mode, this will continue to be called even while the app is in the background (unlike Apple's
+ *     default which is to defer these updates while the app is in the background or to terminate
+ *     your app while in the background).
+ *
+ *     This will also contain location points which have been slightly enhanced by LocationKit by
+ *     filtering outliers, applying some intelligent filters, snapping a bit to the road or to a
+ *     building, or anything else that the situation may merit.
+ */
 - (void)locationManager:(LKLocationManager *)manager
      didUpdateLocations:(NSArray<CLLocation *> *)locations  __OSX_AVAILABLE_STARTING(__MAC_NA,__IPHONE_6_0);
 
@@ -330,7 +437,33 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)locationManager:(LKLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading;
 - (BOOL)locationManagerShouldDisplayHeadingCalibration:(LKLocationManager *)manager;
 
+/*
+ * didEnterRegion
+ *
+ * Discussion:
+ *     This delegate method is called for any one of 3 different region types:
+ *     - CLBeaconRegion - Apple's region for coming within range of a bluetooth beacon
+ *     - CLCircularRegion - Apple's circular geofence region representing a lat/lng with
+ *       a radius around it
+ *     - LKRegion - LocationKit's region representing the radius around a named venue or
+ *       category of venue.
+ *
+ *     Since it could be any of the above region types, it may be necessary to inspect
+ *     the type of region in your app if your app is listening for different types.
+ *
+ *     This method is called when the user enters a region
+ *
+ */
 - (void)locationManager:(LKLocationManager *)manager didEnterRegion:(CLRegion *)region;
+
+/*
+ * didExitRegion
+ *
+ * Discussion:
+ *     This delegate method is called when the user exits a region. As discussed above,
+ *     this region could be one of 3 types so it may be necessary to inspect it before
+ *     using it if your app is listening to multiple types of regions.
+ */
 - (void)locationManager:(LKLocationManager *)manager didExitRegion:(CLRegion *)region;
 - (void)locationManager:(LKLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region;
 - (void)locationManager:(LKLocationManager *)manager monitoringDidFailForRegion:(nullable CLRegion *)region withError:(NSError *)error;
@@ -341,11 +474,66 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)locationManager:(LKLocationManager *)manager didVisit:(CLVisit *)visit;
 
+/*
+ * didChangeAuthorizationStatus
+ *
+ * Discussion:
+ *     This delegate method is called when the authorization status for the application has
+ *     changed.
+ *
+ *     By default, LocationKit will automatically prompt for "always" location permissions but
+ *     this behavior can be overridden by enabling the "LKOptionWhenInUseOnly" prior to starting
+ *     anything requesting locations (startUpdatingLocations, startMonitoringVisits, etc.).
+ *
+ *     This delegate method will be called when that authorization status changes. This could happen
+ *     in response to user action in your app (such as when the user taps "Allow" to permissions) or
+ *     some other way (such as when the user goes into Settings and disallows Location permissions
+ *     for your app). It is up to you as the developer to handle these cases where location permissions
+ *     have been denied so we always recommend supplying a delegate to handle all cases here.
+ */
 - (void)locationManager:(LKLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status;
 
-// LocationKitCore additions
+// LocationKit additions
+
+/*
+ * didStartVisit
+ *
+ * Discussion:
+ *     If you have enabled visit monitoring (by calling startMonitoringVisits() or
+ *     startMonitoringVisitsWithFilter()), this delegate method will be called with
+ *     information about that visit. You'll get an LKVisit object which has things like
+ *     arrival time, the address of the place, and (if it's relevant) the venue, address,
+ *     events, and more that are associated with that place and a list of alternatives
+ *     in case the place we guess for the user is not quite correct.
+ *
+ *     This is generally called within 90-120 seconds of the user arriving at a venue (as
+ *     it takes some time for us to confirm that they're at a place (and not just strolling
+ *     by or through) and resolve which place they are visiting
+ */
 - (void)locationManager:(LKLocationManager *)manager didStartVisit:(LKVisit *)visit;
+
+/*
+ * didEndVisit
+ *
+ * Discussion:
+ *     If you have enabled visit monitoring, this delegate method will be called when a
+ *     user leaves a place. Note, due to GPS drift, didEndVisit is not quite as timely
+ *     as didStartVisit.
+ */
 - (void)locationManager:(LKLocationManager *)manager didEndVisit:(LKVisit *)visit;
+
+/*
+ * willChangeActivityMode
+ *
+ * Discussion:
+ *     This delegate method is called if you have visit monitoring enabled and the user
+ *     is changing activity modes. For example, if they are changing from walking to driving
+ *     or from still to walking, this will be called with the activity mode to which LocationKit
+ *     is about to switch.
+ *
+ *     This is very useful paired with the `setOperationMode` method on LocationKit to alter the
+ *     behavior based on the user's activity.
+ */
 - (void)locationManager:(LKLocationManager *)manager willChangeActivityMode:(LKActivityMode)mode;
 - (void)locationManager:(LKLocationManager *)manager changeRegion:(NSString *)obj;
 
--- Pods/LocationKit/LocationKit.framework/Headers/LKVenue.h
+++ Pods/LocationKit/LocationKit.framework/Headers/LKVenue.h
@@ -8,10 +8,44 @@
 
 @interface LKVenue : NSObject <NSCoding>
 
+/*
+ * venueId
+ *
+ * Discussion:
+ *     The unique venueId of this place in the SocialRadar backend.
+ */
 @property (nonatomic, strong) NSString *venueId;
+
+/*
+ * addressId
+ *
+ * Discussion:
+ *     The unique addressId of this place in the SocialRadar backend.
+ */
 @property (nonatomic, strong) NSString *addressId;
+
+/*
+ * name
+ *
+ * Discussion:
+ *     The name of this place. (e.g. "Capitol City Brewing Company")
+ */
 @property (nonatomic, strong) NSString *name;
+
+/*
+ * category
+ *
+ * Discussion:
+ *     The category of this place. (e.g. "Bars and Nightlife")
+ */
 @property (nonatomic, strong) NSString *category;
+
+/*
+ * subcategory
+ *
+ * Discussion:
+ *     The subcategory of this place (e.g. "Breweries")
+ */
 @property (nonatomic, strong) NSString *subcategory;
 
 @end
